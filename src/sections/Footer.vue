ab169c174a133991f543855718cb023fe698c800
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-08-14 11:21:16 +0300

    first commit

---
+++ src/sections/Footer.vue
@@ -0,0 +1,386 @@
+<template>
+  <div class="section--footer">
+    <div class="container">
+      <div class="footer--inner">
+        <div class="left-side">
+          <img :src="Logo" alt="" class="logo">
+          <div class="left-side__content">
+            <div class="footer__info-list">
+              <span class="footer__info-span">ИНН - 0000876867678</span>
+              <span class="footer__info-span">ОРГН - 0000876867678</span>
+            </div>
+            <span class="footer__list-title">Категории услуг:</span>
+            <ul class="footer__list">
+              <li class="footer__item">Юридическое право</li>
+              <li class="footer__item">Семейное право</li>
+              <li class="footer__item">Административное право</li>
+              <li class="footer__item">Автомобильное право</li>
+              <li class="footer__item">Земельное право</li>
+              <li class="footer__item">Банкротство и кредиты</li>
+            </ul>
+          </div>
+        </div>
+        <div class="right-side">
+          <div class="text">
+            <b>Контакты</b>
+            <p>Найми - юридические услуги</p>
+          </div>
+          <div class="text">
+            <b>name@name24.ru</b>
+            <b>+7 (800) 775 02 13</b>
+            <p>г Найми, ул. Найми 5/1</p>
+          </div>
+          <div class="text">
+            <b>График работы:</b>
+            <p>ПН-ПТ - с 8:00 до 19:00</p>
+            <p>СБ - с 10:00 до 15:00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="container">
+        <p>© Все права защищены ООО “Name” 2023 </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      Logo: "../public/img/Logo.svg",
+      El: "../public/img/Footer/el.svg"
+    };
+  },
+}
+</script>
+
+<style scoped>
+.section--footer {
+  margin-top: 265px;
+  background-color: var(--dark-);
+  padding-top: 168px;
+}
+
+.footer--inner {
+  gap: 100px;
+  color: var(--white-);
+  margin-bottom: 168px;
+}
+
+.left-side p {
+  line-height: 25px;
+  text-indent: 25px;
+}
+
+.right-side img {
+  scale: .7;
+}
+
+.footer--inner {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+  max-width: 50%;
+}
+
+.left-side__content {
+  margin-top: 60px;
+}
+
+.footer__info-list {
+  margin-bottom: 60px;
+}
+
+.footer__info-span,
+.footer__list-title {
+  display: block;
+  font-size: 28px;
+}
+
+.footer__info-span:not(:last-child) {
+  margin-bottom: 25px;
+}
+
+.footer__list-title {
+  margin-bottom: 25px;
+}
+
+.footer__list {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 18px;
+}
+
+.left-side form {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.left-side form input {
+  align-items: center;
+  padding: 25px;
+  gap: 10px;
+  width: 100%;
+  max-width: 450px;
+  background: var(--white-);
+  border-radius: 25px;
+  border: 1px solid var(--white-);
+}
+
+.left-side p {
+  font-size: 22px;
+  line-height: 35px;
+}
+
+.left-side b {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 49px;
+  display: flex;
+  align-items: center;
+}
+
+.right-side {
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+}
+
+.text {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.text b {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 130%;
+}
+
+.text p {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  opacity: 0.5;
+}
+
+.copyright {
+  background-color: #111111;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 0;
+}
+
+.copyright p {
+  color: var(--white-);
+  font-size: 20px;
+  line-height: 1.3;
+  text-align: center;
+}
+
+@media screen and (max-width: 1200px) {
+  .section--footer {
+    padding-top: 100px;
+    margin-top: 180px;
+    text-align: center;
+  }
+
+  .footer--inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 70px;
+    margin-bottom: 100px;
+  }
+
+  .left-side {
+    max-width: 100%;
+  }
+
+  .right-side {
+    gap: 50px;
+  }
+
+  .left-side img {
+    margin: 0 auto;
+  }
+
+  .left-side p {
+    font-size: 16px;
+    line-height: 25px;
+  }
+
+  .text {
+    align-items: center;
+    gap: 15px;
+  }
+
+  .text b {
+    font-size: 36px;
+  }
+
+  .text p {
+    font-size: 22px;
+  }
+
+  .footer__info-span,
+  .footer__list-title {
+    font-size: 26px;
+  }
+
+  .right-side img {
+    scale: 1;
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 992px) {
+  .footer--inner {
+    gap: 50px;
+  }
+
+  .text b {
+    font-size: 32px;
+  }
+
+  .text p {
+    font-size: 20px;
+  }
+
+  .left-side__content {
+    margin-top: 50px;
+  }
+
+  .footer__info-span,
+  .footer__list-title {
+    font-size: 24px;
+  }
+
+  .footer__info-list {
+    margin-bottom: 45px;
+  }
+
+  .footer__info-span:not(:last-child) {
+    margin-bottom: 15px;
+  }
+}
+
+@media (max-width: 768px) {
+  .section--footer {
+    padding-top: 70px;
+    margin-top: 100px;
+  }
+
+  .footer--inner {
+    margin-bottom: 70px;
+  }
+
+  .footer__info-span,
+  .footer__list-title {
+    font-size: 22px;
+  }
+
+  .text b {
+    font-size: 28px;
+  }
+
+  .text p {
+    font-size: 18px;
+  }
+
+  .copyright p {
+    font-size: 18px;
+  }
+}
+
+@media (max-width: 600px) {
+  .section--footer {
+    padding-top: 60px;
+    margin-top: 70px;
+  }
+
+  .footer--inner {
+    margin-bottom: 60px;
+  }
+
+  .footer__info-span,
+  .footer__list-title {
+    font-size: 20px;
+  }
+
+  .text b {
+    font-size: 26px;
+  }
+
+  .text p {
+    font-size: 16px;
+  }
+
+  .copyright {
+    padding: 30px 0;
+  }
+
+  .copyright p {
+    font-size: 16px;
+  }
+
+  .right-side img {
+    width: 250px;
+    height: 270px;
+  }
+
+  .copyright p {
+    font-size: 16px;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .section--footer {
+    padding-top: 50px;
+  }
+
+  .footer--inner {
+    margin-bottom: 50px;
+  }
+
+  .footer--inner {
+    gap: 40px;
+  }
+
+  .footer__info-span,
+  .footer__list-title {
+    font-size: 18px;
+  }
+
+  .footer__list {
+    font-size: 16px;
+  }
+
+  .text b {
+    font-size: 22px;
+  }
+
+  .copyright p {
+    font-size: 14px;
+  }
+}
+</style>
---
+++ src/sections/SectionAbout.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="section--about inner--header">
+    <div class="container">
+      <div class="section--about--inner">
+        <div class="title">
+          <span class="title__number">02</span>
+          <div class="title__content">
+            <p>Правовая поддержка</p>
+            <div class="title__progress">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div class="inner--main">
+          <div class="main--left-side">
+            <img :src="vector" alt="Декоративный элемент" width="330" height="330">
+          </div>
+          <div class="main--right-side">
+            <b>Мы понимаем, что в современном быстро меняющемся мире, правовые вопросы могут стать серьезным
+              препятствием для развития вашего бизнеса.</b>
+            <p>Поэтому наша задача – помочь вам минимизировать риски и достичь успеха, предлагая комплексный
+              набор юридических услуг, адаптированных под ваши потребности.</p>
+            <a v-scroll-to.scrollTo="{ el: '#contact' }" class="btn">
+              <div class="btn--arrow">
+                <svg width="19" height="10" viewBox="0 0 19 10" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M17.9419 5.44194C18.186 5.19786 18.186 4.80214 17.9419 4.55806L13.9645 0.580583C13.7204 0.336505 13.3247 0.336505 13.0806 0.580583C12.8365 0.82466 12.8365 1.22039 13.0806 1.46447L16.6161 5L13.0806 8.53553C12.8365 8.77961 12.8365 9.17534 13.0806 9.41942C13.3247 9.6635 13.7204 9.6635 13.9645 9.41942L17.9419 5.44194ZM0.5 5.625H17.5V4.375H0.5V5.625Z"
+                      fill="#F9FBFE"/>
+                </svg>
+              </div>
+              Заполнить заявку
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueScrollTo from "vue-scrollto";
+
+export default {
+  name: "SectionAbout",
+  directives: {
+    scrollTo: VueScrollTo,
+  },
+  data() {
+    return {
+      vector: "../public/img/sectionAbout/Vector.svg"
+    };
+  }
+}
+</script>
+
+<style scoped>
+.inner--main {
+  margin-top: 120px;
+  display: flex;
+  gap: 120px;
+}
+
+.main--left-side {
+  max-width: 330px;
+  width: 100%;
+}
+
+.main--right-side {
+  max-width: 1000px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.main--right-side b {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 1.2;
+  display: flex;
+  align-items: center;
+  font-feature-settings: 'kern' off;
+  margin-bottom: 50px;
+  color: var(--blue-secondary-);
+}
+
+.main--right-side p {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 24px;
+  color: var(--dark-);
+  opacity: 0.75;
+  margin-bottom: 75px;
+}
+
+@media screen and (max-width: 1400px) {
+  .inner--main {
+    gap: 60px;
+  }
+
+  .main--right-side b {
+    font-size: 26px;
+  }
+
+  .main--right-side p {
+    font-size: 20px;
+    margin-bottom: 40px;
+  }
+
+  .main--left-side img {
+    width: 290px;
+    height: 290px;
+  }
+}
+
+@media (max-width: 1200px) {
+  .inner--main {
+    margin-top: 90px;
+  }
+
+  .main--right-side b {
+    font-size: 24px;
+    margin-bottom: 30px;
+  }
+
+  .main--right-side p {
+    font-size: 18px;
+    margin-bottom: 40px;
+  }
+}
+
+@media screen and (max-width: 992px) {
+  .inner--main {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .main--right-side .btn,
+  .main--left-side img {
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .inner--main {
+    margin-top: 70px;
+    gap: 45px;
+  }
+
+  .main--right-side b {
+    font-size: 22px;
+    margin-bottom: 30px;
+  }
+
+  .main--right-side p {
+    font-size: 18px;
+  }
+
+  .main--left-side img {
+    width: 250px;
+    height: 250px;
+  }
+}
+
+@media (max-width: 600px) {
+  .inner--main {
+    gap: 35px;
+  }
+
+  .main--right-side b {
+    font-size: 20px;
+    margin-bottom: 30px;
+  }
+
+  .main--right-side p {
+    font-size: 16px;
+  }
+
+  .main--left-side img {
+    width: 220px;
+    height: 220px;
+  }
+}
+
+@media screen and (max-width: 468px) {
+  .main--right-side b {
+    font-size: 18px;
+    margin-bottom: 30px;
+  }
+
+  .main--left-side img {
+    width: 190px;
+    height: 190px;
+  }
+}
+
+</style>
